7518e12fc776ac12f7835e5623b51f6ee18e07c3
Kevin Daly <krd313@gmail.com>
2024-01-21 13:48:36 +1100

    2nd login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,74 @@
 
 
 	
-<h1>Hi from login</h1>
+	<!--wrapper-->
+	<div class="wrapper">
+		<div class="section-authentication-cover">
+			<div class="">
+				<div class="row g-0">
 
+					<div class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex">
+
+                        <div class="card shadow-none bg-transparent shadow-none rounded-0 mb-0">
+							<div class="card-body">
+                                 <img src="{{ asset('backend/assets/images/login-images/ned.jpg') }}" class="img-fluid auth-img-cover-login" width=100% alt=""/>
+							</div>
+						</div>
+						
+					</div>
+
+					<div class="col-12 col-xl-5 col-xxl-4 auth-cover-right align-items-center justify-content-center">
+						<div class="card rounded-0 m-3 shadow-none bg-transparent mb-0">
+							<div class="card-body p-sm-5">
+								<div class="">
+									<div class="mb-3 text-center">
+										<img src="assets/images/logo-icon.png" width="60" alt="">
+									</div>
+									<div class="text-center mb-4">
+										<h5 class="">Stuff</h5>
+										<p class="mb-0">Please log in to your account</p>
+									</div>
+									<div class="form-body">
+
+							<form class="row g-3" method="POST"  action="{{ route('login') }}">
+									@csrf
+
+
+										
+											<div class="col-12">
+												<label for="email" class="form-label">Email</label>
+												<input type="email" class="form-control" name="email" id="email" placeholder="jhon@example.com">
+											</div>
+											<div class="col-12">
+												<label for="inputChoosePassword" class="form-label">Password</label>
+												<div class="input-group" id="show_hide_password">
+													<input type="password" class="form-control border-end-0" name="password" id="inputChoosePassword" placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class="bx bx-hide"></i></a>
+												</div>
+											</div>
+
+
+											<div class="col-12">
+												<div class="d-grid">
+													<button type="submit" class="btn btn-primary">Sign in</button>
+												</div>
+											</div>
+
+										</form>
+									</div>
+
+
+
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<!--end row-->
+			</div>
+		</div>
+	</div>
+	<!--end wrapper-->
 
 
 @endsection
